e81661ea71aa4031b1868af6282b9bfa3b65daf1
EmilioRaygozaIbarra <emilio.raygoza@alumnos.udg.mx>
2020-09-29 13:17:22 -0400

    Numero e

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import(
+  "fmt"
+  "math"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+  var e float64
+  var n float64
+  var x float64
+  fmt.Scanln(&x)
+  for n=1;n<=x;n++{
+    e=(1+1/n)
+    e=math.Pow(e,n)
+  }
+  fmt.Println(e)
 }
